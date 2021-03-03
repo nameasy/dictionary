029ebbffed176e28164aa54d03872bb6a75f158a
Stepan Lyubavtsev <nameasy@mail.ru>
2021-03-03 23:10:18 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    var header = document.querySelector(".header");
+    let headroom = new Headroom(header);
+    headroom.init()
+}());
+$(document).ready(function() {
+	$(".main").css("min-height", $(window).outerHeight(!0) - $(".header").outerHeight(!0) - $(".footer").outerHeight(!0))
+});
